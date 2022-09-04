b6af47abb48b91874dcdd4a8dd507c251f7831c7
Arling Holguin <arlingholguin@gmail.com>
2022-09-04 14:48:23 -0500

    Ajustes

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 // console.log("Hola mundo");
+
 //codigo para el modal
 const openModal = document.querySelector(".hero__cta");
 const modal = document.querySelector(".modal");
@@ -39,81 +40,33 @@ closeModal.addEventListener("click", (e) => {
 //     }, 100); //this sets the speed of the animation
 // }, 2000);
 
+//funcion para traer todas las categorias de la api de opentdb https://opentdb.com/api_config.php
+
 // funcion para consultar api https://opentdb.com/api_config.php
 
-const getData = async(amount) => {
-    const url = `https://opentdb.com/api.php?amount=10&category=10&difficulty=easy&type=multiple`;
+let category = document.getElementById("category");
+console.log(category);
+
+const getData = async(amount, category, difficulty, type) => {
+    const url = `https://opentdb.com/api.php?amount=${amount}&category=${category}&difficulty=${difficulty}&type=${type}`;
     const data = await fetch(url);
     const dataJSON = await data.json();
-    return dataJSON.results;
+    let myQuestions = dataJSON.results;
+    return myQuestions;
+};
 
-}
-getData().then((data) => {
-    console.log(data);
-}).catch((error) => {
-    console.log(error);
-}).finally(() => {
-    console.log("finally");
-});
-
-//funcion para crear preguntas y respuestas
-const createQuestions = (data) => {
-    const questions = data.map((question) => {
-        return {
-            question: question.question,
-            answers: question.incorrect_answers.concat(question.correct_answer),
-            correctAnswer: question.correct_answer,
-            correct: null
-        };
-    }).sort(() => Math.random() - 0.5);
-    return questions;
-}
-
-//funcion para crear las preguntas y respuestas en el html
-const createQuiz = (questions) => {
-    const quizContainer = document.getElementById("quiz");
-    const resultsContainer = document.getElementById("results");
-    const submitButton = document.getElementById("submit");
-    const myQuestions = createQuestions(questions);
-    let myScore = 0;
-    const output = [];
-    for (let i = 0; i < myQuestions.length; i++) {
-        const answers = [];
-        for (let j = 0; j < myQuestions[i].answers.length; j++) {
-            answers.push(
-                `<label>
-                    <input type="radio" name="question${i}" value="${myQuestions[i].answers[j]}">
-                    ${myQuestions[i].answers[j]}
-                </label>`
-            );
-        }
-        output.push(
-            `<div class="question"> ${myQuestions[i].question} </div>
-            <div class="answers"> ${answers.join("")} </div>`
-        );
-    }
-    quizContainer.innerHTML = output.join("");
-    submitButton.addEventListener("click", (e) => {
-        e.preventDefault();
-        myScore = 0;
-        for (let i = 0; i < myQuestions.length; i++) {
-            const answerContainer = quizContainer.querySelector(`#question${i}`);
-            const answer = answerContainer.querySelector("input[name=question" + i + "]:checked");
-            if (answer) {
-                if (answer.value === myQuestions[i].correctAnswer) {
-                    myScore++;
-                    answerContainer.style.color = "green";
-                } else {
-                    answerContainer.style.color = "red";
-                }
-            }
-            if (!answer) {
-                answerContainer.style.color = "black";
-            }
-        }
-        resultsContainer.innerHTML = `${myScore} out of ${myQuestions.length}`;
+getData(10, 10, "medium", "multiple")
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+    .finally(() => {
+        console.log("finally");
     });
-}
+
+// funcion para crear el quiz donde pueda sellecionar categoria, dificultad y tipo de pregunta, desde el modal
 
 //funcion para crear el modal con el resultado del quiz
 const createModal = (myScore, myQuestions) => {
@@ -133,4 +86,4 @@ const createModal = (myScore, myQuestions) => {
         modalButton.innerHTML = "Try Again";
     }
     modalContainer.classList.add("modal--show");
-}
+};
