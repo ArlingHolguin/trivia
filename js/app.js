d07d139b31b96446235b67aaefab5a65aa471f93
Arling Holguin <arlingholguin@gmail.com>
2022-09-05 20:28:47 -0500

    Ajustes

--- js/app.js
+++ js/app.js
@@ -44,18 +44,111 @@ closeModal.addEventListener("click", (e) => {
 
 // funcion para consultar api https://opentdb.com/api_config.php
 
-let category = document.getElementById("category");
-console.log(category);
 
-const getData = async(amount, category, difficulty, type) => {
-    const url = `https://opentdb.com/api.php?amount=${amount}&category=${category}&difficulty=${difficulty}&type=${type}`;
+
+//CARGAR CATEGORIAS
+function cargarCategoria() {
+    // let categorias = document.getElementById('categorias');
+    const url = "https://opentdb.com/api_category.php";
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            let html = "";
+            data.trivia_categories.forEach((categoria) => {
+                html += `<option value="${categoria.id}">${categoria.name}</option>`;
+            });
+            document.getElementById("categoria").innerHTML = html;
+        });
+}
+
+// generar url a partir de campos de category type y diffculty
+const generateURL = () => {
+    const category = document.getElementById("categoria").value;
+    const type = document.getElementById("type").value;
+    const difficulty = document.getElementById("difficulty").value;
+    const url = `https://opentdb.com/api.php?amount=10&category=${category}&type=${type}&difficulty=${difficulty}&encode=url3986`;
+    return url;
+};
+
+
+
+//cargar preguntas y respuestas de la api_config.php
+function cargarPreguntas() {
+    const url = generateURL();
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            let html = "";
+            data.results.forEach((pregunta) => {
+                html += `<div class="pregunta">
+                <h2>${pregunta.question}</h2>
+                <ul>
+                    <li>${pregunta.correct_answer}</li>
+                    <li>${pregunta.incorrect_answers[0]}</li>
+                    <li>${pregunta.incorrect_answers[1]}</li>
+                    <li>${pregunta.incorrect_answers[2]}</li>
+                </ul>
+            </div>`;
+            });
+            document.getElementById("preguntas").innerHTML = html;
+        });
+}
+
+
+
+
+// console.log(cargarCategoria());
+
+// //CARGAR DIFICULTAD
+// function cargarDificultad() {
+//     const url = "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple";
+//     fetch(url)
+//         .then((response) => response.json())
+//         .then((data) => {
+//             let html = "";
+//             data.results.forEach((dificultad) => {
+//                 html += `<option value="${dificultad.difficulty}">${dificultad.difficulty}</option>`;
+//             });
+//             document.getElementById("dificultad").innerHTML = html;
+//         });
+// }
+
+// //CARGAR TIPO
+// function cargarTipo() {
+//     const url = "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple";
+//     fetch(url)
+//         .then((response) => response.json())
+//         .then((data) => {
+//             let html = "";
+//             data.results.forEach((tipo) => {
+//                 html += `<option value="${tipo.type}">${tipo.type}</option>`;
+//             });
+//             document.getElementById("tipo").innerHTML = html;
+//         });
+// }
+
+
+
+
+
+
+
+let category = document.getElementById("category").value;
+let difficulty = document.getElementById("difficulty").value;
+let type = document.getElementById("type").value;
+console.log(category);
+console.log(difficulty);
+console.log(type);
+
+const getData = async(category, difficulty, type) => {
+    const url = `https://opentdb.com/api.php?amount=10&category=${category}&difficulty=${difficulty}&type=${type}`;
     const data = await fetch(url);
     const dataJSON = await data.json();
     let myQuestions = dataJSON.results;
     return myQuestions;
 };
 
-getData(10, 10, "medium", "multiple")
+getData(category, difficulty, type)
     .then((data) => {
         console.log(data);
     })
@@ -66,24 +159,28 @@ getData(10, 10, "medium", "multiple")
         console.log("finally");
     });
 
+console.log(getData(category, difficulty, type));
+
 // funcion para crear el quiz donde pueda sellecionar categoria, dificultad y tipo de pregunta, desde el modal
 
 //funcion para crear el modal con el resultado del quiz
-const createModal = (myScore, myQuestions) => {
-    const modalContainer = document.getElementById("modal");
-    const modalContent = document.getElementById("modal-content");
-    const modalTitle = document.getElementById("modal-title");
-    const modalText = document.getElementById("modal-text");
-    const modalButton = document.getElementById("modal-button");
-    const myScorePercent = (myScore / myQuestions.length) * 100;
-    if (myScorePercent >= 80) {
-        modalTitle.innerHTML = "Congratulations!";
-        modalText.innerHTML = "You got " + myScorePercent + "% correct";
-        modalButton.innerHTML = "Play Again";
-    } else {
-        modalTitle.innerHTML = "Sorry!";
-        modalText.innerHTML = "You got " + myScorePercent + "% correct";
-        modalButton.innerHTML = "Try Again";
-    }
-    modalContainer.classList.add("modal--show");
-};
+// const createModal = (myScore, myQuestions) => {
+//     const modalContainer = document.getElementById("modal");
+//     const modalContent = document.getElementById("modal-content");
+//     const modalTitle = document.getElementById("modal-title");
+//     const modalText = document.getElementById("modal-text");
+//     const modalButton = document.getElementById("modal-button");
+//     const myScorePercent = (myScore / myQuestions.length) * 100;
+//     if (myScorePercent >= 80) {
+//         modalTitle.innerHTML = "Congratulations!";
+//         modalText.innerHTML = "You got " + myScorePercent + "% correct";
+//         modalButton.innerHTML = "Play Again";
+//     } else {
+//         modalTitle.innerHTML = "Sorry!";
+//         modalText.innerHTML = "You got " + myScorePercent + "% correct";
+//         modalButton.innerHTML = "Try Again";
+//     }
+//     modalContainer.classList.add("modal--show");
+// };
+
+//funcion para quitar espacios de una cadena de texto
