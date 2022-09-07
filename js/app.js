2a85a3c2793fc6664cc19a49cd83f1fd0a2208b8
Arling Holguin <arlingholguin@gmail.com>
2022-09-06 19:01:43 -0500

    juste carga preguntas

--- js/app.js
+++ js/app.js
@@ -44,8 +44,6 @@ closeModal.addEventListener("click", (e) => {
 
 // funcion para consultar api https://opentdb.com/api_config.php
 
-
-
 //CARGAR CATEGORIAS
 function cargarCategoria() {
     // let categorias = document.getElementById('categorias');
@@ -61,105 +59,113 @@ function cargarCategoria() {
         });
 }
 
-// generar url a partir de campos de category type y diffculty
-const generateURL = () => {
-    const category = document.getElementById("categoria").value;
-    const type = document.getElementById("type").value;
-    const difficulty = document.getElementById("difficulty").value;
-    const url = `https://opentdb.com/api.php?amount=10&category=${category}&type=${type}&difficulty=${difficulty}&encode=url3986`;
-    return url;
-};
+// cargar variable category desde select con oel evento onchange
+var category = document.getElementById("category");
+category.addEventListener("change", (e) => {
+    console.log(e.target.value);
+    category = e.target.value;
+    return category;
+});
+//cargar variable difficulty desde select con el evento onchange
+var difficulty = document.getElementById("difficulty");
+difficulty.addEventListener("change", (e) => {
+    console.log(e.target.value);
+    difficulty = e.target.value;
+    return difficulty;
+});
+//cargar variable type desde select con el evento onchange
+var type = document.getElementById("type");
+type.addEventListener("change", (e) => {
+    console.log(e.target.value);
+    type = e.target.value;
+    // console.log(e.target.selectedIndex + " hola "); // 1
+    return type;
+});
 
+const question = document.getElementById("question");
+const options = document.getElementById("answers_list");
 
-
-//cargar preguntas y respuestas de la api_config.php
-function cargarPreguntas() {
-    const url = generateURL();
-    fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-            let html = "";
-            data.results.forEach((pregunta) => {
-                html += `<div class="pregunta">
-                <h2>${pregunta.question}</h2>
-                <ul>
-                    <li>${pregunta.correct_answer}</li>
-                    <li>${pregunta.incorrect_answers[0]}</li>
-                    <li>${pregunta.incorrect_answers[1]}</li>
-                    <li>${pregunta.incorrect_answers[2]}</li>
-                </ul>
-            </div>`;
-            });
-            document.getElementById("preguntas").innerHTML = html;
-        });
-}
-
-
-
-
-// console.log(cargarCategoria());
-
-// //CARGAR DIFICULTAD
-// function cargarDificultad() {
-//     const url = "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple";
-//     fetch(url)
-//         .then((response) => response.json())
-//         .then((data) => {
-//             let html = "";
-//             data.results.forEach((dificultad) => {
-//                 html += `<option value="${dificultad.difficulty}">${dificultad.difficulty}</option>`;
-//             });
-//             document.getElementById("dificultad").innerHTML = html;
-//         });
-// }
-
-// //CARGAR TIPO
-// function cargarTipo() {
-//     const url = "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple";
-//     fetch(url)
-//         .then((response) => response.json())
-//         .then((data) => {
-//             let html = "";
-//             data.results.forEach((tipo) => {
-//                 html += `<option value="${tipo.type}">${tipo.type}</option>`;
-//             });
-//             document.getElementById("tipo").innerHTML = html;
-//         });
-// }
-
-
-
-
-
-
-
-let category = document.getElementById("category").value;
-let difficulty = document.getElementById("difficulty").value;
-let type = document.getElementById("type").value;
-console.log(category);
-console.log(difficulty);
-console.log(type);
-
-const getData = async(category, difficulty, type) => {
-    const url = `https://opentdb.com/api.php?amount=10&category=${category}&difficulty=${difficulty}&type=${type}`;
+const getData = async() => {
+    const url = `https://opentdb.com/api.php?amount=1&category=${category}&difficulty=${difficulty}&type=${type}`;
     const data = await fetch(url);
     const dataJSON = await data.json();
     let myQuestions = dataJSON.results;
-    return myQuestions;
+    console.log(url);
+    // console.log(myQuestions);
+    // return myQuestions;
+    showMyQuestions(myQuestions[0]);
 };
 
-getData(category, difficulty, type)
-    .then((data) => {
-        console.log(data);
-    })
-    .catch((error) => {
-        console.log(error);
-    })
-    .finally(() => {
-        console.log("finally");
-    });
+function showMyQuestions(dataJSON) {
+    let correct_answer = dataJSON.correct_answer;
+    let incorrect_answers = dataJSON.incorrect_answers;
+    let optionsList = incorrect_answers;
+    optionsList.splice(
+        Math.floor(Math.random() * (incorrect_answers.length + 1)),
+        0,
+        correct_answer
+    );
+    // console.log(correct_answer);
+    // console.log(incorrect_answers);
+    // console.log(incorrect_answers);
+    // console.log(optionsList);
+    question.innerHTML = `${dataJSON.question} `;
+    options.innerHTML = `${optionsList.map((option, index) => `
+        <div class="container_respuesta hvr-bounce-in">
+                <div class="b_circle">
+                    <small>${index + 1}</small>
+                </div>
+                <div class="text-respuesta">
+                    ${option}
+                </div>
+        </div>
+            
+        `
+          ).join("")}
+    `;
+}
 
-console.log(getData(category, difficulty, type));
+
+// getData();
+
+// funcion para mostrar las preguntas
+// const showQuestions = async() => {
+//     const myQuestions = await getData();
+//     let html = "";
+//     myQuestions.forEach((pregunta) => {
+//         html += `<div class="bloque_pregunta">
+//         <h2 class="pregunta">${pregunta.question}</h2>
+//         <div class="bloque_respuestas">
+//             <button class="respuesta">${pregunta.correct_answer}</button>
+//             <button class="respuesta">${pregunta.incorrect_answers[0]}</button>
+//             <button class="respuesta">${pregunta.incorrect_answers[1]}</button>
+//             <button class="respuesta">${pregunta.incorrect_answers[2]}</button>
+//         </div>
+//     </div>`;
+//     });
+//     question.innerHTML = html;
+//     // console.log(myQuestions);
+// };
+
+// };
+
+// funcion para obtener datos de api y controlar los errores
+// function getData() {
+//     console.log(category.length + difficulty.length + type.length);
+//     if (category.length > 0 && difficulty.length > 0 && type.length > 0) {
+//         const url = `https://opentdb.com/api.php?amount=10&category=${category}&difficulty=${difficulty}&type=${type}`;
+//         fetch(url)
+//             .then((response) => response.json())
+//             .then((data) => {
+//                 let myQuestions = data.results;
+//                 console.log(url);
+//                 console.log(myQuestions);
+//                 return myQuestions;
+//             });
+//     } else {
+//         alert('Selecciona una categoria, dificultad y tipo');
+//     }
+// }
 
 // funcion para crear el quiz donde pueda sellecionar categoria, dificultad y tipo de pregunta, desde el modal
 
